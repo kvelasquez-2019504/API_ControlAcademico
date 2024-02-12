0df44fa044b3c4402a5f95a88c0f13ce21ed2ead
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-12 11:56:16 -0600

    Se creo el modelo estudiante

---
+++ models/estudiante.js
@@ -0,0 +1,34 @@
+const {Schema, model}=require('mongoose');
+
+const EstudianteSchema=Schema({
+    nombres:{
+        type:String,
+        required:[true, "Tus nombres son obligatorios"]
+    },
+    apellidos:{
+        type:String,
+        required:[true, "Los apellidos son obligarorios"]
+    },
+    correo:{
+        type:String,
+        required:[true, "El correo es obligatorio"]
+    },
+    grado:{
+        type:String,
+        required:[true,"El grado es requerido"]
+    },
+    edad:{
+        type:Number,
+        required:[true,"La edad es requerida"]
+    },
+    cantidadCursos:{
+        type:Number,
+        defaul:0
+    },
+    cursosAsignados:{
+        type:String,
+        default:""
+    }
+});
+
+module.exports=Estudiante;
