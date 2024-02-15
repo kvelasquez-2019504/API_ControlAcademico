bfcb705789539edae544f76becbd12c77ba136e5
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-15 17:53:22 -0600

    Se creo estudiantesGet

--- controllers/estudiante.controller.js
+++ controllers/estudiante.controller.js
@@ -2,5 +2,19 @@ const {response,json} = require('express');
 const Estudiante = require('../models/estudiante');
 
 const estudiantesGet = async (req,res=response)=>{
-
+    const {limite, desde}=req.query;
+    const query = {estado:true};
+    const [total, estudiantes]= await Promise.all([
+        Estudiante.countDocument(query),
+        Estudiante.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ]);
+    res.status(200).json({
+        total,
+        estudiantes
+    });
+}
+module.exports={
+    estudiantesGet
 }
