2750dffa9e003ecf15dcdd7126e461626f808de0
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-16 01:50:06 -0600

    Se creo el modelo maestros y su controller

---
+++ models/maestros.js
@@ -0,0 +1,34 @@
+const {Schema,model}= require('mongoose');
+
+const MaestroSchema = ({
+    nombres:{
+        type:String,
+        required:[true, "Tus nombres son obligatorios"]
+    },
+    apellidos:{
+        type:String,
+        required:[true, "Los apellidos son obligarorios"]
+    },
+    correo:{
+        type:String,
+        required:[true, "El correo es obligatorio"]
+    },
+    grado:{
+        type:String,
+        required:[true,"El grado es requerido"]
+    },
+    edad:{
+        type:Number,
+        required:[true, "La edad es obligatoria"]
+    },
+    cursos:{
+        type:String,
+        default:[""]
+    },
+    estado:{
+        type:Boolean,
+        default:true
+    }
+});
+
+module.exports = model('Maestro', MaestroSchema);
